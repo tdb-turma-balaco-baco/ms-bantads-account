fb54953bf3c28e6807fa34030fb114155dfae765
mateus <imw.desenv@gmail.com>
2023-01-15 19:58:06 -0300

    feat: folder structure
    Dockerfile

--- src/main/java/br/net/dac/account/AccountApplication.java
+++ src/main/java/br/net/dac/account/AccountApplication.java
@@ -2,8 +2,9 @@ package br.net.dac.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AccountApplication {
 
 	public static void main(String[] args) {
